a6ee4f6c1b816ea0990c0e004b2b700922f96876
chayansrk64 <chayansrk64@gmail.com>
2025-11-12 19:24:22 +0600

    categories api created

--- index.js
+++ index.js
@@ -67,6 +67,12 @@ async function run() {
         res.send(result)
     })
 
+    app.get('/categories', async(req, res) => {
+        const cursor = listingCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
