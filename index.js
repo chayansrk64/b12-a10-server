86d0ad391732600b05962ace22c508c5d4501327
chayansrk64 <chayansrk64@gmail.com>
2025-11-13 11:41:30 +0600

    orders get api created

--- index.js
+++ index.js
@@ -111,6 +111,17 @@ async function run() {
         res.send(result)
     })
 
+    app.get('/orders', async(req, res) => {
+        const email = req.query.email;
+        const query = {}
+        if(email){
+          query.email = email
+        }
+        const cursor = orderCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
